7d482b4ea644afd246280f015c58d9f6e5933375
subu8098 <subuvarathan8098@gmail.com>
2025-08-04 11:47:21 +0530

    ROR Basics

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,71 @@
+class ArticlesController < ApplicationController
+  # def index
+  #   # For now, hardcoded data
+  #   @articles = [
+  #     { title: "First Article", body: "This is the first article body" },
+  #     { title: "Second Article", body: "This is the second article body" }
+  #   ]
+  # end
+
+  # def show
+  #   # Just placeholder logic for now
+  #   @article = { title: "Sample Article", body: "More detailed content..." }
+  # end
+
+  # def new
+  #   render json: { message: "New article form" }
+  # end
+
+  # def edit
+  # end
+
+
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path, notice: "Article deleted"
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
+
+
+end
